170e63245e016129af2f20551692ccb690c29fd9
Mohit-893 <mohit@bhavnacorp.com>
2022-08-25 12:39:32 +0530

    25 aug 2022 (Task-1)

---
+++ Week-1/Day-2/Task-1/Task-1/Overloading.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_1
+{
+    public class Overloading
+    {
+        public void sum()
+        {
+            Console.WriteLine("Enter any number");
+        }
+        public void sum(int a)
+        {
+            Console.WriteLine("Enter two numbers");
+        }
+        public void sum(int a, int b)
+        {
+            Console.WriteLine("Sum is : "+(a+b));
+        }
+        public void sum(int a, int b,int c)
+        {
+            Console.WriteLine("Sum is : " + (a + b + c));
+        }
+
+    }
+}
---
+++ Week-1/Day-2/Task-1/Task-1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 5; i++)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine("");
+            }
+
+            // Armstrong
+
+            Console.WriteLine("Enter any number you want to check for : ");
+            int n = int.Parse(Console.ReadLine());
+            int num = n, r, s = 0;
+            for (; n != 0;)
+            {
+                r = n % 10;
+                n = n / 10;
+                s = s + (r * r * r);
+            }
+            if (num == s)
+            {
+                Console.WriteLine("ArmStrong ");
+            }
+            else
+            {
+                Console.WriteLine("Not Armstrong !!!");
+            }
+
+            //Overloading
+
+            Overloading od = new Overloading();
+            od.sum();
+            od.sum(1);
+            od.sum(2, 3);
+            od.sum(5, 10, 15);
+
+        }
+    }
+}
