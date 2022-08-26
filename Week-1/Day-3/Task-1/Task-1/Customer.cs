000f8e42945c18e68dc2d369ba6026a8f899d966
Mohit-893 <mohit@bhavnacorp.com>
2022-08-26 13:58:02 +0530

    26 Aug 2022 (Task-1)

---
+++ Week-1/Day-3/Task-1/Task-1/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_1
+{
+    class Customer :Classid, Idisplay, Ienterdata
+    {
+        string name;
+        string add;
+        double phone;
+        public void display()
+        {
+            Console.WriteLine("Customer details are : "+id+" "+name+" "+add+" "+phone);
+        }
+
+        public void enterdata()
+        {
+            Console.Write("Enter Customer Id : ");
+            id = int.Parse(Console.ReadLine());
+            Console.Write("Enter Customer Name : ");
+            name = Console.ReadLine();
+            Console.Write("Enter Customer Address : ");
+            add = Console.ReadLine();
+            Console.Write("Enter Customer Phone Number : ");
+            phone = double.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ Week-1/Day-3/Task-1/Task-1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your choice\n1 for Customer\n2 for Product\n3 for Order");
+            int num = int.Parse(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    Customer cs = new Customer();
+                    cs.enterdata();
+                    cs.display();
+                    break;
+                case 2:
+                    Product p = new Product();
+                    p.enterdata();
+                    p.display();
+                    break;
+                case 3:
+                    Order o = new Order();
+                    o.enterdata();
+                    o.display();
+                    break;
+                default:
+                    Console.WriteLine("Please Choose a Valid input !!!");
+                    break;
+            }
+        }
+    }
+}
