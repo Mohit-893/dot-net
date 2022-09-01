b0cf8888006af6d5a7a1560ea42181b666b2ab73
Mohit-893 <mohit@bhavnacorp.com>
2022-09-01 09:14:31 +0530

    31 Aug 2022 (Task-1)

---
+++ Week-2/Day-3/Task_1/StudentData/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentData
+{
+    public class Student
+    {
+        public int roll_no { get; set; }
+        public string name { get; set; }
+        public string clas { get; set; }
+       
+    }
+}
---
+++ Week-2/Day-3/Task_1/Task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con = new SqlConnection("server=localhost;database=school;integrated security=true;");
+            StudentData.Student st = new StudentData.Student();
+            Console.Write("Enter name : ");
+            st.name = Console.ReadLine();
+            Console.Write("Enter Class : ");
+            st.clas = Console.ReadLine();
+            SqlCommand cmd = new SqlCommand("insert into student values('"+st.name+"','"+st.clas+"')", con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Console.WriteLine("Student data added");
+        }
+    }
+}
