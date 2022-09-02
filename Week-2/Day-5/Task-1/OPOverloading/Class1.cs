362010a32bdd75062636bb4e2b3b662cf9a838e7
Mohit-893 <mohit@bhavnacorp.com>
2022-09-02 16:49:15 +0530

    02 Sep 2022 (Task-1(Practice))

---
+++ Week-2/Day-5/Task-1/OPOverloading/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OPOverloading
+{
+    public class Class1
+    {
+        public int no = 0;
+        public Class1(int n)
+        {
+            no = n;
+        }
+        public static Class1 operator + (Class1  num1,Class1 num2)
+        {
+            Class1 num3 = new Class1(0);
+            num3.no = num2.no * num1.no;
+            return num3;
+        }
+    }
+}
---
+++ Week-2/Day-5/Task-1/Task-1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Task_1
+{
+    class person
+    {
+        public int age;
+    }
+    class Program
+    {
+        static void square(person a,person b)
+        {
+            a.age = a.age * a.age;
+            b.age = b.age * b.age;
+            Console.WriteLine(a.age + " " + b.age);
+        }
+        static void Main(string[] args)
+        {
+            person p1 = new person();
+            person p2 = new person();
+            p1.age = 5;
+            p2.age = 10;
+            Console.WriteLine(p1.age + " " + p2.age);
+            square(p1, p2); // reference type
+            Console.WriteLine(p1.age + " " + p2.age);
+
+            Object obj = new Object();
+            int i = 10;
+
+            Type t1 = obj.GetType();
+            Type t2 = i.GetType();
+
+            Console.WriteLine(t1.BaseType);
+            Console.WriteLine(t1.Name);
+            Console.WriteLine(t1.FullName);
+            Console.WriteLine(t1.Namespace);
+            Console.WriteLine("");
+
+        /*
+            Console.WriteLine(t2.BaseType);
+            Console.WriteLine(t2.Name);
+            Console.WriteLine(t2.FullName);
+            Console.WriteLine(t2.Namespace);
+            Console.WriteLine("");
+            //int a = int.Parse(Console.ReadLine());
+
+            string ch = "mohit";
+            Type t4 = ch.GetType();
+            Console.WriteLine(t4.BaseType);
+            Console.WriteLine(t4.Name);
+            Console.WriteLine(t4.FullName);
+            Console.WriteLine(t4.Namespace);
+            Console.WriteLine("");
+
+            List<string> ob = new List<string>();
+            Type t3 = ob.GetType();
+            Console.WriteLine(t3.BaseType);
+            Console.WriteLine(t3.Name);
+            Console.WriteLine(t3.FullName);
+            Console.WriteLine(t3.Namespace);
+        */
+
+            OPOverloading.Class1 num1 = new OPOverloading.Class1(0);
+            OPOverloading.Class1 num2 = new OPOverloading.Class1(0);
+            OPOverloading.Class1 num3 = new OPOverloading.Class1(0);
+
+            Console.Write("Enter First number : ");
+            num1.no = int.Parse(Console.ReadLine());
+            Console.Write("Enter Second number : ");
+            num2.no = int.Parse(Console.ReadLine());
+
+            num3 = num2 + num1;
+            Console.WriteLine("Falsy Sum of {0} and {1} is : {2}", num1.no, num2.no, num3.no);
+
+        }
+    }
+}
