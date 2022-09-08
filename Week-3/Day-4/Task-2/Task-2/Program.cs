6c077c314056828b7d4bc797abe38091bf351df5
Mohit-893 <mohit@bhavnacorp.com>
2022-09-08 17:02:38 +0530

    (Task-)2

---
+++ Week-3/Day-4/Task-2/Task-2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to our Project");
+            Console.WriteLine("1-Help\n2-Start\n3-Setting\n4-Exit");
+            Console.WriteLine("");
+        }
+    }
+}
