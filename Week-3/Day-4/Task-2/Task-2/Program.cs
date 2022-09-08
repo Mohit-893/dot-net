fcf8b85f592c42e436bdabcef4cd139afe23a714
Mohit-893 <mohit@bhavnacorp.com>
2022-09-08 19:30:17 +0530

    08 Sep 2022(Task-2)

--- Week-3/Day-4/Task-2/Task-2/Program.cs
+++ Week-3/Day-4/Task-2/Task-2/Program.cs
@@ -2,13 +2,79 @@
 
 namespace Task_2
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to our Project");
-            Console.WriteLine("1-Help\n2-Start\n3-Setting\n4-Exit");
-            Console.WriteLine("");
+        startpoint:
+            Console.WriteLine("\t\t\t <----- Welcome to My Project ----->\t\t\t");
+            Console.WriteLine("\t\t\t Press : \n 1 - Help\n2 - Start\nn3 - setting\nn4 - Exit \n \t\t\t");
+            string Start;
+            Start = Console.ReadLine();
+            if (Start == "1")
+            {
+                Console.Clear();
+                Console.WriteLine("\n=============================Press : *\\n==============================\n");
+                string gostart = Console.ReadLine();
+                if (gostart == "*")
+                {
+                    Console.Clear();
+                    goto startpoint;
+                }
+            }
+            else if (Start == "2")
+            {
+                Console.Clear();
+                Console.WriteLine("============================\n1-Calculator\n2-  elGadwaldrab\n============================");
+
+
+
+               string gostart = Console.ReadLine();
+                if (gostart == "*")
+                {
+                    Console.Clear();
+                    goto startpoint;
+                }
+            }
+            else if (Start == "3")
+            {
+                Console.Clear();
+                Console.WriteLine("Setting\\interface\nColour\n*black");
+                string interfacechoose = Console.ReadLine();
+                if (interfacechoose =="i")
+                {
+                    Console.Clear();
+                    colorsetting:
+                    Console.WriteLine("Setting\\interface\n1-White\n2-blue/n3-red\n*-back");
+                    string color;
+                    color = Console.ReadLine();
+                    if (color =="1")
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.Clear();
+
+
+
+                   }
+                    if (color == "2")
+                    {
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                    }
+                    if (color == "3")
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        goto startpoint;
+                    }
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
