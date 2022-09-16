f212e760418b206ac795ce5476d32bdf179af1b3
Mohit-893 <mohit@bhavnacorp.com>
2022-09-16 12:58:04 +0530

    16 Sep 2022(Task-1(SnakeApp))

---
+++ Week-4/Day-5/SimpleConsoleSnake/SimpleConsoleSnake/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace SimpleConsoleSnake
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Snake snake = new Snake();
+            while (true)
+            {
+                snake.WriteBorad();
+                snake.Input();
+                snake.Logic();
+            }
+            Console.ReadKey();
+        }
+    }
+}
