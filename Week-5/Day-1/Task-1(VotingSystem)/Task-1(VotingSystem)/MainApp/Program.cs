de986eed72ee3c8d25afd5b2a5da793d8817e37a
Mohit-893 <mohit@bhavnacorp.com>
2022-09-19 15:49:56 +0530

    18 Sep 2022

---
+++ Week-5/Day-1/Task-1(VotingSystem)/Task-1(VotingSystem)/MainApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Task_1_VotingSystem_.UI;
+
+namespace Task_1_VotingSystem_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            start:
+            string aadhar = Console.ReadLine();
+            string pan = Console.ReadLine();
+            if (Functions.isvaliduser(aadhar,pan))
+            {
+                Functions.showCandidateList();
+                var choice = int.Parse(Console.ReadLine());
+                Functions.incrementVote(choice);
+                Functions.updateCandidateData(aadhar,pan);
+            }
+            else
+            {
+                Console.WriteLine("Please Enter correct details");
+                goto start;
+            }
+            
+        }
+
+        
+       
+
+    }
+}
---
+++ Week-5/Day-1/Task-1(VotingSystem)/Task-1(VotingSystem)/UI/Functions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_1_VotingSystem_.UI
+{
+    class Functions
+    {
+
+        public static void showCandidateList()
+        {
+            Console.Clear();
+            Console.WriteLine("\t-------------------------------------------------\t");
+            Console.WriteLine("\t|\tPress\t\t\t\t\t|\t");
+            Console.WriteLine("\t|\t1 for A\t\t\t\t\t|\t");
+            Console.WriteLine("\t|\t2 for B\t\t\t\t\t|\t");
+            Console.WriteLine("\t|\t3 for C\t\t\t\t\t|\t");
+            Console.WriteLine("\t|\t4 for D\t\t\t\t\t|\t");
+            Console.WriteLine("\t|\t5 for E\t\t\t\t\t|\t");
+            Console.WriteLine("\t-------------------------------------------------\t");
+        }
+
+        internal static void updateCandidateData(string aadhar, string pan)
+        {
+            // update isvoted to true
+        }
+
+        internal static void incrementVote(int choice)
+        {
+            // increment voterlist table 
+        }
+
+        internal static bool isvaliduser(string aadhar, string pan)
+        {
+            // data match from database 
+            // isvoted = false
+            return true;
+        }
+    }
+}
