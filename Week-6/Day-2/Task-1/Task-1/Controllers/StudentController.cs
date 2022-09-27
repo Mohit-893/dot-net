f73ac09268b7791b496181e96ef3b2caa97976f4
Mohit-893 <mohit@bhavnacorp.com>
2022-09-27 14:11:58 +0530

    27 Sep 2022(Task-1)

---
+++ Week-6/Day-2/Task-1/Task-1/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Task_1.Models;
+
+namespace Task_1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            Student stu = new Student();
+            Student abc = stu.GetStudentbyId();
+            return View(abc);
+        }
+    }
+}
---
+++ Week-6/Day-2/Task-1/Task-1/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task_1.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public int RollNo { get; set; }
+        public string ClassSection { get; set; }
+        public Student GetStudentbyId()
+        {
+            return new Student { StudentId = 13, Name = "Mohit", RollNo = 17, ClassSection = "VII A" };
+        }
+    }
+}
