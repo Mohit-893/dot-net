a546ad770006e0b3efdf96341e52b5cad477ee70
Mohit-893 <mohit@bhavnacorp.com>
2022-09-29 14:37:42 +0530

    28 Sep 2022

---
+++ Week-6/Day-3/CRUDApp/CRUDApp/Controllers/DuplicateController.cs
@@ -0,0 +1,74 @@
+﻿using CRUDApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDApp.Controllers
+{
+    public class DuplicateController : Controller
+    {
+            private readonly StudentContext _context;
+
+            public DuplicateController(StudentContext context)
+            {
+                _context = context;
+            }
+
+
+            public IActionResult Index()
+            {
+                var stu = _context.Students.ToList();
+                return View(stu);
+            }
+            public IActionResult Details(int id)
+            {
+                var stu = _context.Students.Find(id);
+                return View(stu);
+            }
+
+            public IActionResult Create()
+            {
+
+                return View();
+            }
+
+            [HttpPost]
+            public IActionResult Create(Student student)
+            {
+                _context.Students.Add(student);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+
+            public IActionResult DeleteStudent(int id)
+            {
+                var student = _context.Students.Find(id);
+                _context.Students.Remove(student);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            public IActionResult Delete(int id)
+            {
+                var stu = _context.Students.Find(id);
+                return View(stu);
+            }
+
+            public IActionResult Edit(int id)
+            {
+                var student = _context.Students.Find(id);
+
+                return View(student);
+            }
+
+            public IActionResult EditStudents(Student student)
+            {
+                _context.Entry(student).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+    }
+}
---
+++ Week-6/Day-3/CRUDApp/CRUDApp/Models/StudentContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDApp.Models
+{
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
